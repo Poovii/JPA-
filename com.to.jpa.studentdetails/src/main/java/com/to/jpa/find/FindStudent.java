95fe20259cc4988ef1d4222bdd63e7ba1b55c7bc
D <104251994+Poovii@users.noreply.github.com>
2022-12-07 19:21:32 +0530

    Add files via upload
    Sample programs

---
+++ com.to.jpa.studentdetails/src/main/java/com/to/jpa/find/FindStudent.java
@@ -0,0 +1,28 @@
+package com.to.jpa.find;
+
+import com.to.jpa.studentdetails.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class FindStudent {
+
+	public static void main(String[] args) {
+		 EntityManagerFactory emf=Persistence.createEntityManagerFactory("Student_details");  
+	        EntityManager em=emf.createEntityManager();  
+	        
+	        
+	          
+	      
+	          
+	        StudentEntity s=em.find(StudentEntity.class,101);  
+	          
+	        System.out.println("Student id = "+s.getS_id());  
+	        System.out.println("Student Name = "+s.getS_name());  
+	        System.out.println("Student Age = "+s.getS_age());  
+	          
+	
+	}
+
+}
